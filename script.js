52f61ea3bff8be96b0ba3fa97442befe625f227c
mobindaz <mobindasche@gmail.com>
2023-12-26 10:45:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+const images = [`pic4.jpg`,'pic1.jpg', `pic3.jpg`, `pic5.jpg`, `pic2.jpg`];
+const alts = {
+  'pic1.jpg' : 'Nature',
+  'pic2.jpg' : 'Nature',
+  'pic3.jpg' : 'Nature',
+  'pic4.jpg' : 'Nature',
+  'pic5.jpg' : 'Nature',
+}
+
+
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
